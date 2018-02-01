ea6a454f22e686248c8bb87711217b5412f22d72
Hakim Male <malehakim@gmail.com>
2018-02-01 21:22:44 +0100

    Add files via upload
    Updated files adhering to the exercise specifications

--- tables.sql
+++ tables.sql
@@ -2,67 +2,67 @@ DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
 CREATE TABLE public.program(
-    	"name" VARCHAR(50) NOT NULL,
-	abbreviation CHAR(4) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    abbreviation CHAR(4) NOT NULL,
 	CONSTRAINT program_name_pkey PRIMARY KEY ("name")
 );
 
 CREATE TABLE public.department(
-    	"name" VARCHAR(50) NOT NULL,
-    	abbreviation CHAR(4) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    abbreviation CHAR(4) NOT NULL,
 	CONSTRAINT department_name_pkey PRIMARY KEY ("name"),
 	CONSTRAINT department_ukey UNIQUE(abbreviation)
 );
 
 CREATE TABLE hosts(
 	department VARCHAR(50) NOT NULL, 
-    	"program" VARCHAR(50) NOT NULL,
-    	CONSTRAINT hosts_pkeys PRIMARY KEY (department, "program"),
-    	CONSTRAINT hosts_department_fkey FOREIGN KEY (department) REFERENCES public.department ("name"),
-    	CONSTRAINT hosts_program_fkey FOREIGN KEY ("program") REFERENCES public.program ("name")
+    "program" VARCHAR(50) NOT NULL,
+    CONSTRAINT hosts_pkeys PRIMARY KEY (department, "program"),
+    CONSTRAINT hosts_department_fkey FOREIGN KEY (department) REFERENCES public.department ("name"),
+    CONSTRAINT hosts_program_fkey FOREIGN KEY ("program") REFERENCES public.program ("name")
 );
 
 CREATE TABLE student(
 	ssn CHAR(12) NOT NULL, 
-    	"name" VARCHAR(50) NOT NULL,
-    	login VARCHAR(15) NOT NULL,
-    	"program" VARCHAR(50) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    login VARCHAR(15) NOT NULL,
+    "program" VARCHAR(50) NOT NULL,
 	CONSTRAINT student_ssn_pkey PRIMARY KEY ("ssn"),
 	CONSTRAINT student_ukey UNIQUE (login),
-    	CONSTRAINT student_ukeys UNIQUE(ssn, "program"),
-    	CONSTRAINT student_program_fkey FOREIGN KEY ("program") REFERENCES public.program ("name")
+    CONSTRAINT student_ukeys UNIQUE(ssn, "program"),
+    CONSTRAINT student_program_fkey FOREIGN KEY ("program") REFERENCES public.program ("name")
 );
 
 CREATE TABLE branch(
-    	"name" VARCHAR(50) NOT NULL,
-    	"program" VARCHAR(50) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "program" VARCHAR(50) NOT NULL,
 	CONSTRAINT branch_pkeys PRIMARY KEY ("name", "program"),
 	CONSTRAINT branch_program_program_fkey FOREIGN KEY ("program") REFERENCES public.program("name")
 );
 
 CREATE TABLE belongsTo(
-    	student CHAR(12) NOT NULL,
-    	branch VARCHAR(50) NOT NULL, 
-    	"program" VARCHAR(50) NOT NULL,
-    	CONSTRAINT belongsto_pkey PRIMARY KEY (student),
+    student CHAR(12) NOT NULL,
+    branch VARCHAR(50) NOT NULL, 
+    "program" VARCHAR(50) NOT NULL,
+    CONSTRAINT belongsto_pkey PRIMARY KEY (student),
 	CONSTRAINT belongsto_student_ssn_fkey FOREIGN KEY (student) REFERENCES student(ssn),
-    	CONSTRAINT belongsto_student_fkeys FOREIGN KEY (student, "program") REFERENCES student("ssn", "program"),
-    	CONSTRAINT belongsTo_branch_fkeys FOREIGN KEY(branch, "program") REFERENCES branch ("name", "program")
+    CONSTRAINT belongsto_student_fkeys FOREIGN KEY (student, "program") REFERENCES student("ssn", "program"),
+    CONSTRAINT belongsTo_branch_fkeys FOREIGN KEY(branch, "program") REFERENCES branch ("name", "program")
 );
 
 CREATE TABLE course(
 	code CHAR(8) NOT NULL, 
-    	"name" VARCHAR(50) NOT NULL,
-    	credits float(1) DEFAULT 0.0 NOT NULL,
-    	department VARCHAR(50) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    credits float(1) DEFAULT 0.0 NOT NULL,
+    department VARCHAR(50) NOT NULL,
 	CONSTRAINT course_code_pkey PRIMARY KEY (code),
-    	CONSTRAINT course_department_fkey FOREIGN KEY ("department") REFERENCES department ("name") 
+    CONSTRAINT course_department_fkey FOREIGN KEY ("department") REFERENCES department ("name") 
 );
 
 CREATE TABLE prerequisite(
 	course VARCHAR(50) NOT NULL,
-    	prerequisite VARCHAR(50) NOT NULL,
-    	CONSTRAINT prerequisite_pkeys PRIMARY KEY ("course", "prerequisite"),
+    prerequisite VARCHAR(50) NOT NULL,
+    CONSTRAINT prerequisite_pkeys PRIMARY KEY ("course", "prerequisite"),
 	CONSTRAINT prerequisite_course_course_fkey FOREIGN KEY (course) REFERENCES course(code),
 	CONSTRAINT prerequisite_course_prerequisite_fkey FOREIGN KEY (prerequisite) REFERENCES course(code)
 );
@@ -74,7 +74,7 @@ CREATE TABLE classification(
 
 CREATE TABLE classified(
 	course CHAR(8) NOT NULL,
-    	classification VARCHAR(50) NOT NULL,
+    classification VARCHAR(50) NOT NULL,
 	CONSTRAINT classified_course_pkey PRIMARY KEY ("course", classification),
 	CONSTRAINT classified_course_course_fkey FOREIGN KEY ("course") REFERENCES course ("code"),
 	CONSTRAINT classified_classification_classification_fkey FOREIGN KEY ("classification") REFERENCES classification ("name")
@@ -82,62 +82,62 @@ CREATE TABLE classified(
 
 CREATE TABLE mandatoryprogram(
 	course CHAR(8) NOT NULL,
-    	"program" VARCHAR(50) NOT NULL,
-    	CONSTRAINT mandatoryprogram_pkeys PRIMARY KEY ("course", "program"),
+    "program" VARCHAR(50) NOT NULL,
+    CONSTRAINT mandatoryprogram_pkeys PRIMARY KEY ("course", "program"),
 	CONSTRAINT mandatoryprogram_course_course_fkey FOREIGN KEY (course) REFERENCES course (code),
 	CONSTRAINT mandatoryprogram_program_program_fkey FOREIGN KEY ("program") REFERENCES public.program ("name")
 );
 
 CREATE TABLE mandatorybranch(
 	course CHAR(8) NOT NULL,
-    	branch VARCHAR(50) NOT NULL,
-    	"program" VARCHAR(50) NOT NULL,
-    	CONSTRAINT mandatorybranch_pkeys PRIMARY KEY ("course", "program", "branch"),
+    branch VARCHAR(50) NOT NULL,
+    "program" VARCHAR(50) NOT NULL,
+    CONSTRAINT mandatorybranch_pkeys PRIMARY KEY ("course", "program", "branch"),
 	CONSTRAINT mandatorybranch_course_course_fkey FOREIGN KEY (course) REFERENCES course(code),
 	CONSTRAINT mandatorybranch_branch_fkeys FOREIGN KEY ("branch", "program") REFERENCES branch ("name", "program")
 );
 
 CREATE TABLE recommendedbranch(
 	course CHAR(8) NOT NULL,
-    	branch VARCHAR(50) NOT NULL,
-    	"program" VARCHAR(50) NOT NULL,
-    	CONSTRAINT recommendedbranch_pkeys PRIMARY KEY ("course", "program", "branch"),
+    branch VARCHAR(50) NOT NULL,
+    "program" VARCHAR(50) NOT NULL,
+    CONSTRAINT recommendedbranch_pkeys PRIMARY KEY ("course", "program", "branch"),
 	CONSTRAINT recommendedbranch_course_course_fkey FOREIGN KEY ("course") REFERENCES course ("code"),
 	CONSTRAINT recommendedbranch_branch_fkeys FOREIGN KEY ("branch", "program") REFERENCES branch ("name", "program")
 );
 
 CREATE TABLE registered (
 	student CHAR(12) NOT NULL,
-    	course VARCHAR(50) NOT NULL,
-    	CONSTRAINT registered_pkeys PRIMARY KEY (student, course),
+    course VARCHAR(50) NOT NULL,
+    CONSTRAINT registered_pkeys PRIMARY KEY (student, course),
 	CONSTRAINT registered_student_ssn_fkey FOREIGN KEY (student) REFERENCES student (ssn),
 	CONSTRAINT registered_course_course_fkey FOREIGN KEY (course) REFERENCES course (code)
 );
 
-CREATE TYPE greid AS ENUM ('U','3', '4', '5');
 CREATE TABLE Taken(
 	student CHAR(12) NOT NULL,
-    	course VARCHAR(50) NOT NULL,
-    	grade greid NOT NULL DEFAULT 'U',
-    	CONSTRAINT taken_pkeys PRIMARY KEY (student, course),
+    course VARCHAR(50) NOT NULL,
+    grade CHAR(1) NOT NULL DEFAULT 'U',
+    CONSTRAINT taken_pkeys PRIMARY KEY (student, course),
 	CONSTRAINT taken_student_ssn_fkey FOREIGN KEY (student) REFERENCES student ("ssn"),
 	CONSTRAINT taken_course_course_fkey FOREIGN KEY (course) REFERENCES course ("code")
 );
 
 CREATE TABLE limitedcourse(
 	code CHAR(8) NOT NULL,
-    	seats SMALLINT DEFAULT 0 NOT NULL,
+    seats SMALLINT DEFAULT 0 NOT NULL,
 	CONSTRAINT limitedcourse_pkey PRIMARY KEY (code),
 	CONSTRAINT limitedcourse_course_course_fkey FOREIGN KEY (code) REFERENCES course (code)
 );
 
 CREATE TABLE waitinglist(
 	student CHAR(12) NOT NULL,
-    	course CHAR(8) NOT NULL,
-    	"position" SMALLINT DEFAULT 0 NOT NULL,
-    	CONSTRAINT waitinglist_pkeys PRIMARY KEY (student, course),
+    course CHAR(8) NOT NULL,
+    "position" SMALLINT DEFAULT 0 NOT NULL,
+    CONSTRAINT waitinglist_pkeys PRIMARY KEY (student, course),
 	CONSTRAINT waitinglist_ukeys UNIQUE(course, "position"),
 	CONSTRAINT waitinglist_student_student_fkey FOREIGN KEY ("student") REFERENCES student ("ssn"),
 	CONSTRAINT waitinglist_limitedcourse_course_fkey FOREIGN KEY (course) REFERENCES limitedcourse (code)
 );
+
 
--- views.sql
+++ views.sql
@@ -1,26 +1,14 @@
 ------------------------------------------------------------------------------------------------------------------------
 -------------------- VIEW StudentFollowing -------------------
 ------------------------------------------------------------------------------------------------------------------------
-CREATE OR REPLACE VIEW StudentFollowing AS 
-	SELECT s.ssn AS student, s.program, b.name AS branch FROM student AS s, branch AS b;
-
-------------------------------------------------------------------------------------------------------------------------
--------------------- Helper Function : Concatenates Course Code and Name --------------------
-------------------------------------------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION ConcatCourseCodeAndCourseName (code CHAR(12), courseName character varying) RETURNS TEXT AS $$
-BEGIN
-	RETURN code::text || ' - ' || courseName::text;
-END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE VIEW StudentsFollowing AS 
+	SELECT s.ssn AS student, s.program, (SELECT branch FROM belongsto b WHERE b.student = s.ssn AND b.program = s.program) AS branch FROM student s;
 
 ------------------------------------------------------------------------------------------------------------------------
 -------------------- VIEW FinishedCourses --------------------
 ------------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE VIEW FinishedCourses AS
-	SELECT s.ssn AS student,
-		ConcatCourseCodeAndCourseName(t.course, c.name) AS course, 
-		(grade::TEXT) AS grade, 
-		CASE WHEN grade = 'U' THEN 0 ELSE credits END AS credits FROM taken as t 
+	SELECT s.ssn AS student,t.course, grade::TEXT, credits FROM taken as t 
 	INNER JOIN student as s ON (s.ssn = t.student)
 	INNER JOIN course as c ON (c.code = t.course);
 
@@ -28,17 +16,17 @@ CREATE OR REPLACE VIEW FinishedCourses AS
 -------------------- VIEW Registration --------------------
 ------------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE  VIEW Registrations AS	
-	SELECT r.student, ConcatCourseCodeAndCourseName(r.course, c.name) AS course, 'Registered' As status 
+	SELECT r.student, r.course, 'registered' As status 
 	FROM registered as r JOIN course c ON(c.code = r.course)
 	UNION 
-	SELECT w.student AS student, ConcatCourseCodeAndCourseName(w.course, c.name) AS course, 'Waiting' As status 
+	SELECT w.student AS student, w.course, 'waiting' As status 
 	FROM waitinglist AS w JOIN course c ON(c.code = w.course);
 
 ------------------------------------------------------------------------------------------------------------------------
 -------------------- VIEW PassedCourses	--------------------
 ------------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE VIEW PassedCourses AS
-	SELECT student, t.course AS course, credits FROM taken t
+	SELECT student, t.course, credits FROM taken t
 	JOIN course c ON c.code = t.course
 WHERE t.grade <> 'U';
 
@@ -46,17 +34,23 @@ WHERE t.grade <> 'U';
 -------------------- VIEW UnreadMandatory --------------------
 ------------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE VIEW UnreadMandatory AS 
-	SELECT k.ssn AS student, array_agg(k.courseslist) AS course
-		FROM ( SELECT s.ssn, m.course::text AS courseslist
-				FROM student s
-				LEFT JOIN mandatoryprogram m USING (program)
-				GROUP BY s.ssn, m.course) k
-	WHERE NOT (k.courseslist IN ( 
-			SELECT t.course
-			FROM taken t
-			WHERE t.student = k.ssn AND t.grade <> 'U'::greid)
-		)
-  GROUP BY k.ssn;
+	SELECT a.ssn AS student, courseslist AS course FROM 
+	(
+		SELECT s.ssn, m.course::text AS courseslist
+			FROM student s
+			LEFT JOIN mandatoryprogram m USING (program)
+		WHERE m.course NOT IN (SELECT t.course FROM taken t WHERE grade <> 'U' AND t.student = s.ssn)
+		GROUP BY s.ssn, m.course
+	UNION
+		SELECT s.ssn, v.course::text AS courseslist
+			FROM student s
+			LEFT JOIN mandatorybranch v USING(program)
+		WHERE branch = (SELECT branch FROM belongsto b WHERE b.student = s.ssn AND b.program = s.program) 
+			AND v.course NOT IN (SELECT t.course FROM taken t WHERE grade <> 'U' AND t.student = s.ssn)
+		GROUP BY s.ssn, v.course
+	) AS a 
+GROUP BY a.ssn, a.courseslist;
+
 
 ------------------------------------------------------------------------------------------------------------------------
 -------------------- Helper Function : classification - Returns an Integer
@@ -67,11 +61,9 @@ DECLARE
 
 BEGIN
 	IF classif <> 'Mathematical' THEN
-		kount := (SELECT COUNT(*) FROM passedcourses AS k 
-			JOIN classified AS h ON (h.course = k.course) WHERE k.student = sssn AND classification = classif);
+		kount := (SELECT COUNT(*) FROM passedcourses JOIN classified USING (course) WHERE student = sssn AND classification = classif);
 	ELSE
-		kount := (SELECT COALESCE(SUM(credits), 0) FROM passedcourses AS k 
-			JOIN classified AS h ON (h.course = k.course) WHERE k.student = sssn AND classification = classif);
+		kount := (SELECT COALESCE(SUM(credits), 0) FROM passedcourses JOIN classified USING (course) WHERE student = sssn AND classification = classif)::integer;
 	END IF;
 	RETURN kount;
 END;
@@ -84,9 +76,32 @@ CREATE OR REPLACE VIEW PathToGraduation AS
 SELECT 
 	s.ssn AS student,
 	(SELECT COALESCE(SUM(credits), 0) AS credo FROM passedcourses AS p WHERE p.student = s.ssn) AS  totalCredits,
-	(SELECT COALESCE(array_length(course, 1), 0) FROM unreadmandatory AS u RIGHT JOIN student g ON g.ssn = u.student WHERE g.ssn = s.ssn) AS mandatoryleft,
-	(SELECT classification('Mathematical', s.ssn)) AS mathCredits,
-	(SELECT classification('Research', s.ssn)) AS researchCredits,
-	(SELECT classification('Seminar', s.ssn)) AS seminarCredits,
-	(SELECT NOT EXISTS (SELECT * FROM unreadmandatory AS u WHERE u.student = s.ssn)) AS status
+	(SELECT COUNT(*) FROM unreadmandatory WHERE student = s.ssn) AS mandatoryLeft,
+	(SELECT COALESCE(SUM(credits),0) FROM passedCourses JOIN classified USING (course) WHERE student = s.ssn AND classification = 'Mathematical')  AS mathCredits,
+	(SELECT COALESCE(SUM(credits),0) FROM passedCourses JOIN classified USING (course) WHERE student = s.ssn AND classification = 'Research') AS researchCredits,
+	(SELECT COUNT(*) FROM passedCourses JOIN classified USING (course) WHERE student = s.ssn AND classification = 'Seminar') AS seminarCourses,
+	(
+		-- Check for all passed courses as per student program
+		(SELECT COUNT(*) FROM passedCourses JOIN mandatoryprogram USING (course) WHERE student = s.ssn AND program = s.program) 
+			= (SELECT COUNT(*) FROM mandatoryprogram WHERE program = s.program) AND
+
+		-- Check for all passed courses as per student program chosen branch
+		(SELECT COUNT(*) FROM passedCourses JOIN mandatorybranch USING (course) WHERE student = s.ssn AND program = s.program) 
+			= (SELECT COUNT(*) FROM mandatorybranch WHERE program = s.program AND branch = 
+				(SELECT branch from belongsto b WHERE b.student = s.ssn AND program = program)) AND
+
+		-- Check that at least 10 Credit Units among recommendedCourses are passed
+		(SELECT COALESCE(SUM(credits),0) >= 10 FROM passedCourses JOIN recommendedbranch USING(course) WHERE student = s.ssn AND program = s.program AND 
+			branch = (SELECT branch from belongsto b WHERE b.student = s.ssn AND program = s.program)) AND
+
+		-- Check that at least 20 Credit Units among Courses classified as Mathematical are passed
+		(SELECT COALESCE(SUM(credits),0) >= 20 FROM passedCourses JOIN classified USING (course) WHERE student = s.ssn AND classification = 'Mathematical') AND
+
+		-- Check that at least 10 Credit Units among Courses classified as Research are passed
+		(SELECT COALESCE(SUM(credits),0) >= 10 FROM passedCourses JOIN classified USING (course) WHERE student = s.ssn AND classification = 'Research') AND
+
+		-- Check that at least 1 Credit Units among Courses classified as Seminar are passed
+		(SELECT COALESCE(SUM(credits),0) >= 1 FROM passedCourses JOIN classified USING (course) WHERE student = s.ssn AND classification = 'Seminar')
+	) AS status
 FROM student AS s;
+
